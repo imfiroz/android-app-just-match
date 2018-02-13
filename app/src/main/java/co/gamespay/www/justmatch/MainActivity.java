ca32fb280c69d4fd0be76c57366e12bd76685878
firoz baksh <ferozbaksh3191@gmail.com>
2018-02-14 00:05:53 +0530

    Home, Game and Score Layout

--- app/src/main/java/co/gamespay/www/justmatch/MainActivity.java
+++ app/src/main/java/co/gamespay/www/justmatch/MainActivity.java
@@ -1,13 +1,50 @@
 package co.gamespay.www.justmatch;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int seconds = 30;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnAction = (Button) findViewById(R.id.btnAction);
+        btnAction.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, GameActivity.class);
+                startActivity(i);
+            }
+        });
+
+        private void timer()
+    {
+        final Handler handler = new Handler();
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                if (seconds>0)
+                {
+                    seconds--;
+                    handler.postDelayed(this, 10000);
+                }
+                else
+                {
+                    //TIME COMPLETE
+                }
+            }
+        })
+    }
+
+
+
     }
 }
