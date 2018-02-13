97609f64dc4a003ca8d90d99f6ec78f6a3cc39ff
firoz baksh <ferozbaksh3191@gmail.com>
2018-02-14 02:43:13 +0530

    Play Button Activity Done

--- app/src/main/java/co/gamespay/www/justmatch/MainActivity.java
+++ app/src/main/java/co/gamespay/www/justmatch/MainActivity.java
@@ -6,45 +6,27 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
-    private int seconds = 30;
+    ImageButton btn_share, btn_play, btn_star;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button btnAction = (Button) findViewById(R.id.btnAction);
-        btnAction.setOnClickListener(new View.OnClickListener() {
+        btn_share   =   (ImageButton) findViewById(R.id.btn_share);
+        btn_play    =   (ImageButton) findViewById(R.id.btn_play);
+        btn_star    =   (ImageButton) findViewById(R.id.btn_star);
+
+        btn_play.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent i = new Intent(MainActivity.this, GameActivity.class);
                 startActivity(i);
             }
         });
-
-        private void timer()
-    {
-        final Handler handler = new Handler();
-        handler.post(new Runnable() {
-            @Override
-            public void run() {
-                if (seconds>0)
-                {
-                    seconds--;
-                    handler.postDelayed(this, 10000);
-                }
-                else
-                {
-                    //TIME COMPLETE
-                }
-            }
-        })
-    }
-
-
-
     }
 }
